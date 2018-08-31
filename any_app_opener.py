d261df4bded53bb2ed95a03c04584d8d3ec2b0a3
Cource <40368016+Cource@users.noreply.github.com>
2018-08-31 20:36:45 +0530

    Opens the app the user tells it to open

--- any_app_opener.py
+++ any_app_opener.py
@@ -4,14 +4,18 @@ import os
 
 app_name = str(input('Enter the name of the app:  '))
 name_list = ('firefox', 'atom', 'unity')
+# add the paths respective to the item number of the app name
+# remember to add the  '\"  at the begining and  \"'  at the end
 path_list=( '\"C:/Program Files/Mozilla Firefox/firefox.exe\"',  '\"%appdata%/Local/atom/atom.exe\"' , \
 '\"D:/Unity/Editor/Unity.exe\"')
-#
+
+# The working:
 x = 0
 while x < len(name_list):
     if app_name == name_list[x]:
         break
+# Adding 1 to x so that the next list item is scanned 
     x += 1
-#
+
 y = path_list[x]
 os.system(y)
